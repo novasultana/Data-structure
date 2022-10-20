ad3462a9d15300e95ee9a61aa78c89c4aaa393de
Nova Sultana <79368259+novasultana@users.noreply.github.com>
2022-10-20 17:48:45 +0600

    Add files via upload

---
+++ sum_of_rows.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int x[50][50];
+    int m,n,i,j;
+    printf("Enter number of Rows: ");
+    scanf("%d",&m);
+
+    printf("Enter number of Columns: ");
+    scanf("%d",&n);
+
+    printf("Enter elements: \n");
+
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&x[i][j]);
+        }
+    }
+
+    printf("Output:\n");
+    int sum=0;
+    for(i=0;i<m;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+          sum=sum+x[i][j];
+        }
+         printf("Sum of Row:%d\n",sum);
+         sum=0;
+
+    }
+
+    return 0;
+}
+
